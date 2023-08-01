8d2316bdaa0ccb90fc9eef17af24e3300176cd90
morispolanco <morispolanco@gmail.com>
2023-08-01 15:53:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,35 @@
 import streamlit as st
-import requests
 import json
+import requests
 
-# Set up the chatbot
-st.title("Streamlit Chatbot")
-
-# Define the API endpoint and headers
-endpoint = "https://api.theforgeai.com/v1/apps/64a8eeb6bd770b33fdae84fc/view"
+# Define the API endpoint, headers and the data
+api_url = "https://api.theforgeai.com/v1/apps/64a8eeb6bd770b33fdae84fc/view/run"
 headers = {
     "X-API-KEY": "sk_RxYG4kgIdeL6N9KdXBD5JIHwhMpPr_FKOfIetVj2y40",
-    # Add Content-Type header for sending JSON data
     "Content-Type": "application/json"
 }
 
-# Define the initial state of the chatbot
-state = {}
+def send_message(message):
+    data = {
+      "user_inputs": {
+        "text_input_3": {
+          "value": message
+        }
+      }
+    }
 
-# Add a text input field to the app
-text_input = st.empty()
-
-# Add placeholder for bot response
-bot_response = st.empty()
-
-# Add a button to send the message to the API
-button_send = st.button("Send Message")
-
-def send_message():
-    text_input_value = text_input.text_input("Enter your message here", "")
-    payload = json.dumps({"user_inputs": {"text_input_3": {"value": text_input_value}}})
     response = requests.post(
-        endpoint,
+        api_url,
         headers=headers,
-        data=payload
+        data=json.dumps(data)
     )
-    bot_response.text("Chatbot: " + response.json()["message"])
 
-# On button click
-if button_send:
-    send_message()
+    return response.json()
+
+# Streamlit app
+st.title('Streamlit Chatbot')
+
+user_message = st.text_input("Enter your message")
+if st.button("Send"):
+    response = send_message(user_message)
+    st.write(response) # Display the response received from the API
