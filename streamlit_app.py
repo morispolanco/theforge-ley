5749b30ff48e2c592bc03c132277bdc198365602
morispolanco <morispolanco@gmail.com>
2023-08-01 15:00:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,43 @@
 import streamlit as st
 import requests
 import json
-import pprint
 
-def obtener_respuesta_api(texto):
-    headers = {"X-API-KEY": "sk_RxYG4kgIdeL6N9KdXBD5JIHwhMpPr_FKOfIetVj2y40", "Content-Type": "application/json"}
-    data = {
-        "user_inputs": {
-            "text_input_3": {
-                "value": texto
+# Configura tu API key
+headers = {
+    "X-API-KEY": "sk_RxYG4kgIdeL6N9KdXBD5JIHwhMpPr_FKOfIetVj2y40",
+    "Content-Type":"application/json"
+}
+
+# URL base de la API
+base_url = "https://api.theforgeai.com/v1/apps/64a8eeb6bd770b33fdae84fc/view"
+
+# Configuración de la aplicación Streamlit
+st.title("Chatbot basado en TheForgeAI")
+
+# Input del usuario
+user_input = st.text_input("Escribe tu mensaje:")
+
+# Cuando el usuario presiona el botón "Enviar", se envía la solicitud a la API
+if st.button("Enviar"):
+    if user_input:
+        data = {
+            "user_inputs": {
+                "text_input_3": {
+                    "value": user_input
+                }
             }
         }
-    }
-    response = requests.post(
-        "https://api.theforgeai.com/v1/apps/64a8eeb6bd770b33fdae84fc/view/run",
-        headers=headers,
-        data=json.dumps(data)
-    )
-    return response.json().get('outputs', {}).get('text_output_4', {}).get('value', 'Error: No se recibió una respuesta válida')
 
-def main():
-    st.title("Chat con API de The Forge AI")
+        response = requests.post(
+            f"{base_url}/run",
+            headers=headers,
+            data=json.dumps(data)
+        )
 
-    # Inicializar la variable donde se guardará el historial de mensajes
-    historial_mensajes = []
-
-    # Cuadro de texto para que el usuario escriba su mensaje
-    mensaje_usuario = st.text_input("Escribe tu mensaje:")
-
-    # Botón para enviar el mensaje
-    if st.button("Enviar"):
-        if mensaje_usuario:
-            historial_mensajes.append(("Tú:", mensaje_usuario))
-            respuesta = obtener_respuesta_api(mensaje_usuario)
-            historial_mensajes.append(("Bot:", respuesta))
-
-    # Mostrar el historial de mensajes en la app
-    if historial_mensajes:
-        st.text("Historial de mensajes:")
-        for remitente, mensaje in historial_mensajes:
-            st.text(f"{remitente} {mensaje}")
-
-if __name__ == "__main__":
-    main()
+        if response.status_code == 200:
+            response_data = response.json()
+            st.write("Respuesta del bot:", response_data.get('outputs', {}).get('text_output_1', {}).get('value', ''))
+        else:
+            st.write("Ha ocurrido un error al procesar la solicitud.")
+    else:
+        st.write("Por favor, escribe un mensaje.")
