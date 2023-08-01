4bc2c0331495cabad416143d105de56b008956ee
morispolanco <morispolanco@gmail.com>
2023-08-01 15:09:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,18 @@ base_url = "https://api.theforgeai.com/v1/apps/64a8eeb6bd770b33fdae84fc/view"
 # Configuración de la aplicación Streamlit
 st.title("Chatbot basado en TheForgeAI")
 
+# Creación de una lista para almacenar los mensajes del chat
+chat_history = []
+
 # Input del usuario
 user_input = st.text_input("Escribe tu mensaje:")
 
 # Cuando el usuario presiona el botón "Enviar", se envía la solicitud a la API
 if st.button("Enviar"):
     if user_input:
+        # Agrega el mensaje del usuario al historial del chat
+        chat_history.append({"user": user_input})
+
         data = {
             "user_inputs": {
                 "text_input_3": {
@@ -37,8 +43,17 @@ if st.button("Enviar"):
         if response.status_code == 200:
             response_data = response.json()
             bot_response = response_data.get('user_outputs', {}).get('text_output_5', {}).get('value', '')
-            st.write("Respuesta del bot:", bot_response)
+            
+            # Agrega la respuesta del bot al historial del chat
+            chat_history.append({"bot": bot_response})
         else:
             st.write("Ha ocurrido un error al procesar la solicitud.")
     else:
         st.write("Por favor, escribe un mensaje.")
+
+# Muestra el historial del chat
+for chat in chat_history:
+    if "user" in chat:
+        st.write("Usuario: ", chat["user"])
+    else:
+        st.write("Bot: ", chat["bot"])
